a93da663070c8617260d49134bcc3c312d675cf3
Ralf Ueberfuhr <ralf.zahn@dv-ma.de>
2019-03-28 10:46:03 +0100

    Lösung Samples 01 eingepflegt

--- src/main/java/de/ars/schulung/effectivejava/samples01/Einstieg.java
+++ src/main/java/de/ars/schulung/effectivejava/samples01/Einstieg.java
@@ -15,7 +15,23 @@ public class Einstieg {
 	 * Wenn nein, warum nicht?
 	 */
 	public static void main(String[] args) {
-		long l1 = 0x0_cafe_babe;
+		
+		/*
+		 * Erklärung:
+		 *  - 0xCAFE BABE ist 32-bit int mit führender 1 (C = 1101, negative Zahl)
+		 *  - bei Zuweisung an 64-bit long Variable werden führende 1er vorn aufgefüllt
+		 *  - Damit ist die Addition dann
+		 *  
+		 *    FFFF FFFF CAFE BABE
+		 *  + 0000 0001 0000 0000
+		 *    ===================
+		 *    0000 0000 CAFE BABE
+		 * 
+		 * Lösung:
+		 *  - bei Zuweisung direkt einen long-Wert angeben (L)
+		 */
+		
+		long l1 = 0x0_cafe_babeL;
 		long l2 = 0x1_0000_0000L;
 		printHex(() -> l1 + l2);
 	}
